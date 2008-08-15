d6b6190502bb2541bf134380860e92dd80934132
rohan.anil <rohan.anil@23b13dde-1d4e-0410-9555-c5db500eae70>
2008-08-15 15:51:39 +0000

    Importing new files

---
+++ pam_face_authentication/cmd-facetracker.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+void main()
+{   char *c;
+   int shmidCommAuth;
+    key_t ipckeyCommAuth;
+    int *commAuth;
+    key_t ipckey;
+    ipckeyCommAuth =  567813;
+
+    shmidCommAuth = shmget(ipckeyCommAuth, sizeof(int), IPC_CREAT | 0666);
+    commAuth = shmat(shmidCommAuth, NULL, 0);
+printf("---------------------------------------------- \n");
+printf("\n (A)uthenticate or  (C)ancel ? : \n");
+
+scanf("%c",c);
+ *commAuth =14;
+
+if((*c=='A')||(*c=='a'))
+{
+       *commAuth =7;
+    //break;
+}
+
+}
+
+
+
+
+
+
+
