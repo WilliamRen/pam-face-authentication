c72492ca3030b790d004ed1885afff7e930b7cd8
rohan.anil <rohan.anil@23b13dde-1d4e-0410-9555-c5db500eae70>
2009-07-21 02:51:42 +0000

    KDM Greet Plugin

--- kgreet_faceauthenticate.cpp
+++ kgreet_faceauthenticate.cpp
@@ -1,9 +1,14 @@
+
 /*
 
 Conversation widget for kdm greeter
 
 Copyright (C) 1997, 1998, 2000 Steffen Hansen <hansen@kde.org>
 Copyright (C) 2000-2003 Oswald Buddenhagen <ossi@kde.org>
+Copyright (C) 2009 Jaroslav Barton <djaara@djaara.net>
+ 
+ Adapted to PAM Face Authentication - during Google Summer of Code 2009
+Copyright (C) 2009 Rohan Anil <rohan.anil@gmail.com>
 
 
 This program is free software; you can redistribute it and/or modify
@@ -22,34 +27,81 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 
-#include "kgreet_faceauthenticate.h"
-#include "themer/kdmthemer.h"
-#include "themer/kdmitem.h"
 
+#include "kgreet_faceauthenticate.h"
+#include <qapplication.h>
 #include <kglobal.h>
 #include <klocale.h>
 #include <klineedit.h>
 #include <kuser.h>
-
+#include <assert.h>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QImage>
 #include <QRegExp>
 #include <QLayout>
 #include <QLabel>
+#include "webcamQLabel.h"
+#define IPC_KEY_IMAGE 567814
+#define IPC_KEY_STATUS 567813
+#define IMAGE_SIZE 230400
+#define IMAGE_WIDTH 320
+#define IMAGE_HEIGHT 240
+void KFaceAuthenticateGreeter::timerEvent(QTimerEvent * event)
+{
+    static QGraphicsScene * scene = new QGraphicsScene(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+	uchar* pBits         = image.bits();
+    int nBytesPerLine    = image.bytesPerLine();
+    int n,m;
+if(authStarted==true)
+{
 
-static int echoMode;
 
-class KDMPasswordEdit : public KLineEdit {
-public:
-	KDMPasswordEdit( QWidget *parent ) : KLineEdit( parent )
-	{
-		if (::echoMode == -1)
-			setPasswordMode(true);
-		else
-			setEchoMode( ::echoMode ? Password : NoEcho );
-		setContextMenuPolicy( Qt::NoContextMenu );
-	}
-};
 
-KClassic1Greeter::KClassic1Greeter( KGreeterPluginHandler *_handler,
+    for (n=0;n<IMAGE_HEIGHT;n++)
+    {
+        for (m= 0;m<IMAGE_WIDTH;m++)
+        {
+
+            QRgb value;
+            value = qRgb((unsigned char)*(shared + m*3 + 2+ n*IMAGE_WIDTH*3), (unsigned char)*(shared + m*3 + 1+ n*IMAGE_WIDTH*3), (unsigned char)*(shared + m*3 + 0+ n*IMAGE_WIDTH*3));
+            uchar * scanLine = pBits+n*nBytesPerLine;
+            ((uint *)scanLine)[m] = value;
+        }
+    }
+
+}
+else
+{
+
+    for (n=0;n<IMAGE_HEIGHT;n++)
+    {
+        for (m= 0;m<IMAGE_WIDTH;m++)
+        {
+
+            QRgb value;
+            value = qRgb(0,0,0);
+            uchar * scanLine = pBits+n*nBytesPerLine;
+            ((uint *)scanLine)[m] = value;
+        }
+    }
+}
+	
+    webcamPreview->setScene(scene);
+    webcamPreview->setBackgroundBrush(image);
+    webcamPreview->show();
+}
+void KFaceAuthenticateGreeter::ipcStart()
+{
+    /*   IPC   */
+    ipckey =  IPC_KEY_IMAGE;
+    shmid = shmget(ipckey, IMAGE_SIZE, IPC_CREAT | 0666);
+    shared = (char *)shmat(shmid, NULL, 0);
+    /*   IPC END  */ image= QImage(IMAGE_WIDTH, IMAGE_HEIGHT, QImage::Format_RGB32);
+}
+
+
+KFaceAuthenticateGreeter::KFaceAuthenticateGreeter( KGreeterPluginHandler *_handler,
                                   QWidget *parent,
                                   const QString &_fixedEntity,
                                   Function _func, Context _ctx ) :
@@ -58,15 +110,15 @@ KClassic1Greeter::KClassic1Greeter( KGreeterPluginHandler *_handler,
 	fixedUser( _fixedEntity ),
 	func( _func ),
 	ctx( _ctx ),
-	exp( -1 ),
-	pExp( -1 ),
-	running( false )
+	running( false ),
+	authStarted( false )
 {
 	QGridLayout *grid = 0;
 	int line = 0;
 
 	if (!_handler->gplugHasNode( "user-entry" ) ||
-	    !_handler->gplugHasNode( "pw-entry" ))
+	    !_handler->gplugHasNode( "faceauthenticate-status" ) ||
+	    !_handler->gplugHasNode( "webcam-preview" ) )
 	{
 		parent = new QWidget( parent );
 		parent->setObjectName( "talker" );
@@ -74,17 +126,19 @@ KClassic1Greeter::KClassic1Greeter( KGreeterPluginHandler *_handler,
 		grid = new QGridLayout( parent );
 		grid->setMargin( 0 );
 	}
-
-	loginLabel = passwdLabel = passwd1Label = passwd2Label = 0;
+	
+	loginLabel = faceauthenticateStatus =  0;
 	loginEdit = 0;
-	passwdEdit = passwd1Edit = passwd2Edit = 0;
-	if (ctx == ExUnlock || ctx == ExChangeTok)
+
+	if (ctx == ExUnlock)
 		fixedUser = KUser().loginName();
 	if (func != ChAuthTok) {
+
+
+  
 		if (fixedUser.isEmpty()) {
 			loginEdit = new KLineEdit( parent );
 			loginEdit->setContextMenuPolicy( Qt::NoContextMenu );
-			connect( loginEdit, SIGNAL(editingFinished()), SLOT(slotLoginLostFocus()) );
 			connect( loginEdit, SIGNAL(editingFinished()), SLOT(slotChanged()) );
 			connect( loginEdit, SIGNAL(textChanged( const QString & )), SLOT(slotChanged()) );
 			connect( loginEdit, SIGNAL(selectionChanged()), SLOT(slotChanged()) );
@@ -92,65 +146,75 @@ KClassic1Greeter::KClassic1Greeter( KGreeterPluginHandler *_handler,
 				loginEdit->setObjectName( "user-entry" );
 				widgetList << loginEdit;
 			} else {
-				loginLabel = new QLabel( i18n("&Username:"), parent );
+				loginLabel = new QLabel( i18n( "&Username:" ), parent );
 				loginLabel->setBuddy( loginEdit );
 				grid->addWidget( loginLabel, line, 0 );
 				grid->addWidget( loginEdit, line++, 1 );
 			}
-		} else if (ctx != Login && ctx != Shutdown && grid) {
-			loginLabel = new QLabel( i18n("Username:"), parent );
+		} else if (ctx != Login && ctx != Shutdown && ctx != ExUnlock && grid) {
+			loginLabel = new QLabel( i18n( "Username:" ), parent );
 			grid->addWidget( loginLabel, line, 0 );
 			grid->addWidget( new QLabel( fixedUser, parent ), line++, 1 );
 		}
-		passwdEdit = new KDMPasswordEdit( parent );
-		connect( passwdEdit, SIGNAL(textChanged( const QString & )),
-		         SLOT(slotChanged()) );
-		connect( passwdEdit, SIGNAL(editingFinished()), SLOT(slotChanged()) );
+		
+		faceauthenticateStatus = new QLabel();
 		if (!grid) {
-			passwdEdit->setObjectName( "pw-entry" );
-			widgetList << passwdEdit;
+			faceauthenticateStatus->setObjectName( "faceauthenticate-status" );
+			widgetList << faceauthenticateStatus;
+		} else{
+			grid->addWidget( faceauthenticateStatus, line++, 0, 1, 2 );
+		}
+
+		ipcStart();
+		webcamPreview = new QGraphicsView();
+	    webcamPreview->setGeometry(QRect(0, 0, 320, 240));
+        webcamPreview->setMaximumSize(QSize(320, 240));
+        webcamPreview->setFrameShape(QFrame::NoFrame);
+        webcamPreview->setFrameShadow(QFrame::Plain);
+        webcamPreview->setLineWidth(0);
+        webcamPreview->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        webcamPreview->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        webcamPreview->setSceneRect(QRectF(0, 0, 320, 240));
+	
+		if (!grid) {
+			webcamPreview->setObjectName( "webcam-preview" );
+			widgetList << webcamPreview;
 		} else {
-			passwdLabel = new QLabel( func == Authenticate ?
-			                             i18n("&Password:") :
-			                             i18n("Current &password:"),
-			                          parent );
-			passwdLabel->setBuddy( passwdEdit );
-			grid->addWidget( passwdLabel, line, 0 );
-			grid->addWidget( passwdEdit, line++, 1 );
+     		grid->addWidget( webcamPreview, line, 0, 2, 2 );
+
+			
 		}
-		if (loginEdit)
+		 image= QImage(IMAGE_WIDTH, IMAGE_HEIGHT, QImage::Format_RGB32);
+
+		
+		if (loginEdit) {
 			loginEdit->setFocus();
-		else
-			passwdEdit->setFocus();
-	}
-	if (func != Authenticate) {
-		passwd1Edit = new KDMPasswordEdit( parent );
-		passwd1Label = new QLabel( i18n("&New password:"), parent );
-		passwd1Label->setBuddy( passwd1Edit );
-		passwd2Edit = new KDMPasswordEdit( parent );
-		passwd2Label = new QLabel( i18n("Con&firm password:"), parent );
-		passwd2Label->setBuddy( passwd2Edit );
-		if (grid) {
-			grid->addWidget( passwd1Label, line, 0 );
-			grid->addWidget( passwd1Edit, line++, 1 );
-			grid->addWidget( passwd2Label, line, 0 );
-			grid->addWidget( passwd2Edit, line, 1 );
 		}
-		if (!passwdEdit)
-			passwd1Edit->setFocus();
+		
+		if (ctx == ExUnlock) {
+			faceauthenticateStatus->setText( i18n( "Click on Unlock button." ) );
+		}
 	}
 }
+/*
+void KFaceAuthenticateGreeter::timerEvent( QTimerEvent * )
+{
+
+
+}
+*/
+
 
 // virtual
-KClassic1Greeter::~KClassic1Greeter()
+KFaceAuthenticateGreeter::~KFaceAuthenticateGreeter()
 {
 	abort();
 	qDeleteAll( widgetList );
 }
 
 void // virtual
-KClassic1Greeter::loadUsers( const QStringList &users )
-{
+KFaceAuthenticateGreeter::loadUsers( const QStringList &users )
+{	void showText();
 	KCompletion *userNamesCompletion = new KCompletion;
 	userNamesCompletion->setItems( users );
 	loginEdit->setCompletionObject( userNamesCompletion );
@@ -159,295 +223,186 @@ KClassic1Greeter::loadUsers( const QStringList &users )
 }
 
 void // virtual
-KClassic1Greeter::presetEntity( const QString &entity, int field )
+KFaceAuthenticateGreeter::presetEntity( const QString &entity, int field )
 {
 	loginEdit->setText( entity );
-	if (field == 1)
-		passwdEdit->setFocus();
-	else {
+	if (field == 0) {
 		loginEdit->setFocus();
 		loginEdit->selectAll();
-		if (field == -1) {
-			passwdEdit->setText( "     " );
-			passwdEdit->setEnabled( false );
-			authTok = false;
-		}
 	}
 	curUser = entity;
 }
 
 QString // virtual
-KClassic1Greeter::getEntity() const
+KFaceAuthenticateGreeter::getEntity() const
 {
 	return fixedUser.isEmpty() ? loginEdit->text() : fixedUser;
 }
 
 void // virtual
-KClassic1Greeter::setUser( const QString &user )
+KFaceAuthenticateGreeter::setUser( const QString &user )
 {
 	// assert( fixedUser.isEmpty() );
 	curUser = user;
 	loginEdit->setText( user );
-	passwdEdit->setFocus();
-	passwdEdit->selectAll();
 }
 
 void // virtual
-KClassic1Greeter::setEnabled( bool enable )
+KFaceAuthenticateGreeter::setEnabled( bool enable )
 {
-	// assert( !passwd1Label );
 	// assert( func == Authenticate && ctx == Shutdown );
-//	if (loginLabel)
-//		loginLabel->setEnabled( enable );
-	passwdLabel->setEnabled( enable );
+	if (loginLabel) {
+		loginLabel->setEnabled( enable );
+		loginEdit->setEnabled( enable );
+	}
+	
+	faceauthenticateStatus->setEnabled( enable );
+	
 	setActive( enable );
-	if (enable)
-		passwdEdit->setFocus();
 }
 
 void // private
-KClassic1Greeter::returnData()
+KFaceAuthenticateGreeter::returnData()
 {
-	switch (exp) {
-	case 0:
-		handler->gplugReturnText( (loginEdit ? loginEdit->text() :
-		                                       fixedUser).toLocal8Bit(),
-		                          KGreeterPluginHandler::IsUser );
-		break;
-	case 1:
-		Q_ASSERT(passwdEdit);
-		handler->gplugReturnText( passwdEdit->text().toLocal8Bit() ,
-		                          KGreeterPluginHandler::IsPassword |
-		                          KGreeterPluginHandler::IsSecret );
-		break;
-	case 2:
-		Q_ASSERT(passwd1Edit);
-		handler->gplugReturnText( passwd1Edit->text().toLocal8Bit(),
-		                          KGreeterPluginHandler::IsSecret );
-		break;
-	default: // case 3:
-		Q_ASSERT(passwd2Edit);
-		handler->gplugReturnText( passwd2Edit->text().toLocal8Bit(),
-		                          KGreeterPluginHandler::IsNewPassword |
-		                          KGreeterPluginHandler::IsSecret );
-		break;
-	}
+handler->gplugReturnText( (loginEdit ? loginEdit->text():fixedUser).toLocal8Bit(),KGreeterPluginHandler::IsUser );
 }
 
 bool // virtual
-KClassic1Greeter::textMessage( const char *text, bool err )
+KFaceAuthenticateGreeter::textMessage( const char *text, bool err )
 {
-	if (!err &&
-	    QString( text ).indexOf( QRegExp( "^Changing password for [^ ]+$" ) ) >= 0)
+	if(text!=0)
+	{
+		QString msg = QString(text);
+		faceauthenticateStatus->setText( i18n(msg.toLatin1()) );
+		if ( msg.indexOf("Face Verification Pluggable Authentication Module Started" ) > -1 )
+			startTimer(80);
 		return true;
-	return false;
+	}
+
+	return false;	
+
 }
 
 void // virtual
-KClassic1Greeter::textPrompt( const char *prompt, bool echo, bool nonBlocking )
+KFaceAuthenticateGreeter::textPrompt( const char *prompt, bool echo, bool nonBlocking )
 {
-	pExp = exp;
-	if (echo)
-		exp = 0;
-	else if (!authTok)
-		exp = 1;
-	else {
-		QString pr( prompt );
-		if (pr.indexOf( QRegExp( "\\bpassword\\b", Qt::CaseInsensitive ) ) >= 0) {
-			if (pr.indexOf( QRegExp( "\\b(re-?(enter|type)|again|confirm|repeat)\\b",
-			                      Qt::CaseInsensitive ) ) >= 0)
-				exp = 3;
-			else if (pr.indexOf( QRegExp( "\\bnew\\b", Qt::CaseInsensitive ) ) >= 0)
-				exp = 2;
-			else { // QRegExp( "\\b(old|current)\\b", Qt::CaseInsensitive ) is too strict
-				handler->gplugReturnText( "",
-				                          KGreeterPluginHandler::IsOldPassword |
-				                          KGreeterPluginHandler::IsSecret );
-				return;
-			}
-		} else {
-			handler->gplugMsgBox( QMessageBox::Critical,
-			                      i18n("Unrecognized prompt \"%1\"",
-			                        prompt ) );
-			handler->gplugReturnText( 0, 0 );
-			exp = -1;
-			return;
-		}
-	}
-
-	if (pExp >= 0 && pExp >= exp) {
-		revive();
-		has = -1;
-	}
-
-	if (has >= exp || nonBlocking)
-		returnData();
+returnData();
 }
 
 bool // virtual
-KClassic1Greeter::binaryPrompt( const char *, bool )
+KFaceAuthenticateGreeter::binaryPrompt( const char *, bool )
 {
-	// this simply cannot happen ... :}
 	return true;
 }
 
 void // virtual
-KClassic1Greeter::start()
+KFaceAuthenticateGreeter::start()
 {
-	authTok = !(passwdEdit && passwdEdit->isEnabled());
-	exp = has = -1;
 	running = true;
 }
 
 void // virtual
-KClassic1Greeter::suspend()
+KFaceAuthenticateGreeter::suspend()
 {
 }
 
 void // virtual
-KClassic1Greeter::resume()
+KFaceAuthenticateGreeter::resume()
 {
 }
 
 void // virtual
-KClassic1Greeter::next()
+KFaceAuthenticateGreeter::next()
 {
 	// assert( running );
-	if (loginEdit && loginEdit->hasFocus()) {
-		passwdEdit->setFocus(); // will cancel running login if necessary
-		has = 0;
-	} else if (passwdEdit && passwdEdit->hasFocus()) {
-		if (passwd1Edit)
-			passwd1Edit->setFocus();
-		has = 1;
-	} else if (passwd1Edit) {
-		if (passwd1Edit->hasFocus()) {
-			passwd2Edit->setFocus();
-			has = 1; // sic!
-		} else
-			has = 3;
-	} else
-		has = 1;
-	if (exp < 0)
-		handler->gplugStart();
-	else if (has >= exp)
-		returnData();
+	setActive(false);
+	authStarted = true;
+	handler->gplugStart();
+	//returnData();
 }
 
 void // virtual
-KClassic1Greeter::abort()
+KFaceAuthenticateGreeter::abort()
 {
-	running = false;
-	if (exp >= 0) {
-		exp = -1;
+	if ( authStarted ) {
 		handler->gplugReturnText( 0, 0 );
 	}
+	//animLabel->stop();
+	faceauthenticateStatus->clear();
+	authStarted = false;
+	running = false;
 }
 
 void // virtual
-KClassic1Greeter::succeeded()
+KFaceAuthenticateGreeter::succeeded()
 {
 	// assert( running || timed_login );
 	if (!authTok) {
 		setActive( false );
-		if (passwd1Edit) {
-			authTok = true;
-			return;
-		}
-	} else
-		setActive2( false );
-	exp = -1;
+	}
 	running = false;
+	authStarted = false;
+	//animLabel->stop();
+	faceauthenticateStatus->setText(i18n("Succeeded"));
 }
 
 void // virtual
-KClassic1Greeter::failed()
+KFaceAuthenticateGreeter::failed()
 {
 	// assert( running || timed_login );
 	setActive( false );
-	setActive2( false );
-	exp = -1;
+ 	faceauthenticateStatus->clear();
 	running = false;
+	authStarted = false;
+	//animLabel->stop();
 }
 
 void // virtual
-KClassic1Greeter::revive()
+KFaceAuthenticateGreeter::revive()
 {
 	// assert( !running );
-	setActive2( true );
-	if (authTok) {
-		passwd1Edit->clear();
-		passwd2Edit->clear();
-		passwd1Edit->setFocus();
-	} else {
-		passwdEdit->clear();
-		if (loginEdit && loginEdit->isEnabled())
-			passwdEdit->setEnabled( true );
-		else {
-			setActive( true );
-			if (loginEdit && loginEdit->text().isEmpty())
-				loginEdit->setFocus();
-			else
-				passwdEdit->setFocus();
-		}
-	}
+	// assert( authTok );
+	
+	setActive( true );
+	loginEdit->setFocus();
 }
 
 void // virtual
-KClassic1Greeter::clear()
+KFaceAuthenticateGreeter::clear()
 {
-	// assert( !running && !passwd1Edit );
-	passwdEdit->clear();
 	if (loginEdit) {
 		loginEdit->clear();
 		loginEdit->setFocus();
 		curUser.clear();
-	} else
-		passwdEdit->setFocus();
+	}
+	faceauthenticateStatus->clear();
+// 	QApplication::processEvents();
 }
 
 
 // private
 
 void
-KClassic1Greeter::setActive( bool enable )
+KFaceAuthenticateGreeter::setActive( bool enable )
 {
 	if (loginEdit)
 		loginEdit->setEnabled( enable );
-	if (passwdEdit)
-		passwdEdit->setEnabled( enable );
+	//faceauthenticateStatus->setEnabled( enable );
 }
 
 void
-KClassic1Greeter::setActive2( bool enable )
+KFaceAuthenticateGreeter::slotChanged()
 {
-	if (passwd1Edit) {
-		passwd1Edit->setEnabled( enable );
-		passwd2Edit->setEnabled( enable );
-	}
-}
-
-void
-KClassic1Greeter::slotLoginLostFocus()
-{
-	if (!running)
-		return;
-	loginEdit->setText( loginEdit->text().trimmed() );
-	if (exp > 0) {
-		if (curUser == loginEdit->text())
-			return;
-		exp = -1;
-		handler->gplugReturnText( 0, 0 );
-	}
-	curUser = loginEdit->text();
-	handler->gplugSetUser( curUser );
-}
-
-void
-KClassic1Greeter::slotChanged()
-{
-	if (running)
+	if ( running ) {
+		loginEdit->setText( loginEdit->text().trimmed() );
+		
+		if (curUser != loginEdit->text()) {
+			curUser = loginEdit->text();
+			handler->gplugSetUser( curUser );
+		}
+		
 		handler->gplugChanged();
+	}
 }
 
 // factory
@@ -456,14 +411,15 @@ static bool init( const QString &,
                   QVariant (*getConf)( void *, const char *, const QVariant & ),
                   void *ctx )
 {
-	echoMode = getConf( ctx, "EchoPasswd", QVariant( -1 ) ).toInt();
-	KGlobal::locale()->insertCatalog( "kgreet_classic1" );
+	Q_UNUSED(getConf);
+	Q_UNUSED(ctx);
+	KGlobal::locale()->insertCatalog( "kgreet_faceauthenticate" );
 	return true;
 }
 
 static void done( void )
 {
-	KGlobal::locale()->removeCatalog( "kgreet_classic1" );
+	KGlobal::locale()->removeCatalog( "kgreet_faceauthenticate" );
 }
 
 static KGreeterPlugin *
@@ -473,11 +429,11 @@ create( KGreeterPluginHandler *handler,
         KGreeterPlugin::Function func,
         KGreeterPlugin::Context ctx )
 {
-	return new KClassic1Greeter( handler, parent, fixedEntity, func, ctx );
+	return new KFaceAuthenticateGreeter( handler, parent, fixedEntity, func, ctx );
 }
 
 KDE_EXPORT KGreeterPluginInfo kgreeterplugin_info = {
-	I18N_NOOP2("@item:inmenu authentication method", "Username + password (classic1)"), "classic1",
+	I18N_NOOP2("@item:inmenu authentication method", "Username + Face Verification"), "faceauthenticate",
 	KGreeterPluginInfo::Local | KGreeterPluginInfo::Presettable,
 	init, done, create
 };
--- kgreet_faceauthenticate.h
+++ kgreet_faceauthenticate.h
@@ -4,6 +4,8 @@ Conversation widget for kdm greeter
 
 Copyright (C) 1997, 1998 Steffen Hansen <hansen@kde.org>
 Copyright (C) 2000-2003 Oswald Buddenhagen <ossi@kde.org>
+Copyright (C) 2009 Jaroslav Barton <djaara@djaara.net>
+Copyright (C) 2009 Rohan Anil <rohan.anil@gmail.com>, BITS Pilani Goa Campus
 
 
 This program is free software; you can redistribute it and/or modify
@@ -23,26 +25,35 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#ifndef KGREET_CLASSIC1_H
-#define KGREET_CLASSIC1_H
+#ifndef KGREET_FACEAUTHENTICATE_H
+#define KGREET_FACEAUTHENTICATE_H
 
-#include "kgreeterplugin.h"
+#include <kgreeterplugin.h>
 
 #include <QObject>
-
+#include <QGridLayout>
+#include <QTimer>
+#include <QImage>
+#include <QGraphicsView>
+//#include "webcamQLabel.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
 class KLineEdit;
 class KSimpleConfig;
 class QLabel;
 
-class KClassic1Greeter : public QObject, public KGreeterPlugin {
+class KFaceAuthenticateGreeter : public QObject, public KGreeterPlugin {
 	Q_OBJECT
 
   public:
-	KClassic1Greeter( KGreeterPluginHandler *handler,
+	KFaceAuthenticateGreeter( KGreeterPluginHandler *handler,
 	                 QWidget *parent,
 	                 const QString &fixedEntitiy,
 	                 Function func, Context ctx );
-	~KClassic1Greeter();
+	~KFaceAuthenticateGreeter();
 	virtual void loadUsers( const QStringList &users );
 	virtual void presetEntity( const QString &entity, int field );
 	virtual QString getEntity() const;
@@ -60,26 +71,33 @@ class KClassic1Greeter : public QObject, public KGreeterPlugin {
 	virtual void failed();
 	virtual void revive();
 	virtual void clear();
+  protected:
+        void timerEvent(QTimerEvent *event);
 
   public Q_SLOTS:
-	void slotLoginLostFocus();
 	void slotChanged();
-
   private:
 	void setActive( bool enable );
-	void setActive2( bool enable );
 	void returnData();
+	void ipcStart();
+	//void timeout();
+		QImage image;
 
-	QLabel *loginLabel, *passwdLabel, *passwd1Label, *passwd2Label;
+	//webcamQLabel *webcamPreview;
+	QLabel *loginLabel, *faceauthenticateStatus;
+	//QTimer *webcamTimer;
+	QGraphicsView *webcamPreview;
+	QString sensor, finger;
 	KLineEdit *loginEdit;
-	KLineEdit *passwdEdit, *passwd1Edit, *passwd2Edit;
 	KSimpleConfig *stsFile;
 	QString fixedUser, curUser;
 	Function func;
 	Context ctx;
-	int exp, pExp, has;
-	bool running, authTok;
+	int has;
+	bool running, authTok, authStarted;
+     char *shared;
+	key_t ipckey;
+	int shmid;
 };
 
-#endif /* KGREET_CLASSIC1_H */
-// 
+#endif /* KGREET_FACEAUTHENTICATE_H */
