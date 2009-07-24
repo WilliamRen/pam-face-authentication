d1e03385e841afb7134754dca673c0fe5c6ab9cb
rohan.anil <rohan.anil@23b13dde-1d4e-0410-9555-c5db500eae70>
2009-07-24 01:54:27 +0000

    Some more Doxygen Documentation for Headers

--- include/pam_face_authentication.h
+++ include/pam_face_authentication.h
@@ -1,15 +1,29 @@
-////////////////////////////////////////////////////////////////////////
+/**
+* Write Image To Memory
+*@param Input Image
+*@param Shared Memory Pointer
+*/
 void writeImageToMemory(IplImage*,char*);
+/**
+* Start IPC Connections
+*/
 void ipcStart();
+/**
+* Reset IPC Flags
+*/
 void resetFlags();
-////////////////////////////////////////////////////////////////////////
 
-
+/**
+* Pointer for Image Shared Memory
+*/
 char *shared;
 key_t ipckey;
 int shmid;
 
 // COMMUNICATION SHARED MEMORY
+/**
+* Pointer for Communication Shared Memory
+*/
 int *commAuth;
 key_t ipckeyCommAuth;
 int shmidCommAuth;
