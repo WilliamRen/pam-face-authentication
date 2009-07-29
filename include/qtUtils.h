2f9bdfb5b1c0fe028f8013f66946866405ed9279
rohan.anil <rohan.anil@23b13dde-1d4e-0410-9555-c5db500eae70>
2009-07-29 13:20:36 +0000

    Missing header files

---
+++ include/qtUtils.h
@@ -0,0 +1,12 @@
+#include "cv.h"
+#include <QtGui>
+#include "highgui.h"
+#ifndef _INCL_QTUTILS
+#define _INCL_QTUTILS
+/**
+* Convert IplImage to QImage
+*@param input Input Image
+*@result Output QImage
+*/
+QImage *QImageIplImageCvt(IplImage *input);
+#endif
