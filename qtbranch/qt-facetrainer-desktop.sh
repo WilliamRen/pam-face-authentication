c5c455d2c067384a4d400b84b533c28eefadc1cc
Francesco.Marella <Francesco.Marella@23b13dde-1d4e-0410-9555-c5db500eae70>
2010-02-17 16:01:18 +0000

    Fix desktop file.

--- qtbranch/qt-facetrainer-desktop.sh
+++ qtbranch/qt-facetrainer-desktop.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cat <<EOF
 [Desktop Entry]
-Version=0.3
+Version=1.0
 Type=Application
 Name=Qt Face Trainer
 GenericName=Face Trainer
 Comment=Face Trainer for PAM Face Authentication
 TryExec=$1/bin/qt-facetrainer
-Exec=$1/bin/qt-facetrainer &
-Categories=Application;
-Icon=$1/share/icons/pfa-logo.png
+Exec=$1/bin/qt-facetrainer
+Categories=System;
+Icon=pfa-logo
 EOF
