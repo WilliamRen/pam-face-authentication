b8e3296d53a53df578d4e50dc56bc523c7551a33
igascream <igascream@23b13dde-1d4e-0410-9555-c5db500eae70>
2010-07-20 18:14:50 +0000

    Russian translation uploaded

---
+++ qtbranch/trans/pam_face_authentication_ru_RU.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS>
+    <context>
+        <name>src/pam_face_authentication.cpp:525</name>
+        <message>
+            <source>Align your face.</source>
+            <translation>Выровните лицо.</translation>
+        </message>
+    </context>
+    <context>
+        <name>src/pam_face_authentication.cpp:454</name>
+        <message>
+            <source>Biometrics model has not been generated for the user. Use qt-facetrainer to create the model.</source>
+            <translation>Биометрическая модель не была создана для пользователя. Воспользуитесь qt-facetrainer для создания модели.</translation>
+        </message>
+    </context>
+    <context>
+        <name>src/pam_face_authentication.cpp:559</name>
+        <message>
+            <source>Giving Up Face Authentication. Try Again.</source>
+            <translation>Аутентификация лица не удалась. Попробуйте заново.</translation>
+        </message>
+    </context>
+    <context>
+        <name>src/pam_face_authentication.cpp:538</name>
+        <message>
+            <source>Keep proper distance with the camera.</source>
+            <translation>Придерживайтесь надлежашего растояния от камеры.</translation>
+        </message>
+    </context>
+    <context>
+        <name>src/pam_face_authentication.cpp:552</name>
+        <message>
+            <source>Unable query image from your webcam.</source>
+            <translation>Ошибка запроса изображения с камеры.</translation>
+        </message>
+    </context>
+    <context>
+        <name>src/pam_face_authentication.cpp:434</name>
+        <message>
+            <source>Unable to get hold of your webcam. Please check if it is plugged in.</source>
+            <translation>Камера не доступна. Пожалуйста проверьте подключение.</translation>
+        </message>
+    </context>
+    <context>
+        <name>src/pam_face_authentication.cpp:497</name>
+        <message>
+            <source>Verification Successful.</source>
+            <translation>Проверка завершена успешно.</translation>
+        </message>
+    </context>
+    <context>
+        <name>src/pam_face_authentication.cpp:489</name>
+        <message>
+            <source>Verifying Face ...</source>
+            <translation>Идет проверка лица ...</translation>
+        </message>
+    </context>
+</TS>
---
+++ qtbranch/trans/qt_facetrainer_ru_RU.ts
@@ -0,0 +1,256 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS>
+    <context>
+        <name>aboutDialog</name>
+        <message>
+            <source>About the Software \m/</source>
+            <translation>О программе \m/</translation>
+        </message>
+        <message>
+            <source>Pluggable Authentication Module for your favorite Linux Distro</source>
+            <translation>Подключаемый Модуль Аутентификации для вашего люмимого Linux дистрибутива</translation>
+        </message>
+        <message>
+            <source>About</source>
+            <translation>О программе</translation>
+        </message>
+        <message>
+            <source>This program allows you to login by Face Authentication.
+Care has been taken to give you the best User Experience Possible.
+If you encounter any bugs , report at  </source>
+            <translation>Данная программа позволяет вам войти в систему с помощью Лицевой Аутентификации.
+Было приложено много усилий ,чтобы вы получили наилучшие впечатления от использования программы.
+ Если вы обнаружили неполадки , сообщите на </translation>
+        </message>
+        <message>
+            <source>This program was developed during the auspicious Google Summer of Code program in 2009 and 2008
+</source>
+            <translation>Данная программа была разработана за время Google Summer of Code  2008 и 2009 года
+</translation>
+        </message>
+        <message>
+            <source>Authors</source>
+            <translation>Авторы</translation>
+        </message>
+        <message>
+            <source>Thanks To</source>
+            <translation>Благодарности</translation>
+        </message>
+        <message>
+            <source>Translation</source>
+            <translation>Перевод</translation>
+        </message>
+        <message>
+            <source>Artwork</source>
+            <translation>Оформление</translation>
+        </message>
+        <message>
+            <source>OK</source>
+            <translation>ОК</translation>
+        </message>
+    </context>
+    <context>
+        <name>faceTrainer</name>
+        <message>
+            <source>Face Trainer</source>
+            <translation>Обработчик Лица</translation>
+        </message>
+        <message>
+            <source>Next</source>
+            <translation>Далее</translation>
+        </message>
+        <message>
+            <source>For Your Information</source>
+            <translation>Для вашего сведения</translation>
+        </message>
+        <message>
+            <source>1</source>
+            <translation>1</translation>
+        </message>
+        <message>
+            <source>Exit</source>
+            <translation>Выход</translation>
+        </message>
+        <message>
+            <source>color: rgb(139, 139, 139);</source>
+            <translation>color: rgb(139, 139, 139);</translation>
+        </message>
+        <message>
+            <source>Read the instructions for High Efficiency</source>
+            <translation>Прочтите инструкции для Лучшей Продуктивности</translation>
+        </message>
+        <message>
+            <source>color: rgb(126, 126, 126);</source>
+            <translation>color: rgb(139, 139, 139);</translation>
+        </message>
+        <message>
+            <source>To ensure high efficiency please keep proper distance between face and webcam</source>
+            <translation>Для лу</translation>
+        </message>
+        <message>
+            <source>Try to sit where there is ample lighting</source>
+            <translation>Постарайтесь находится на хорошо освещенном месте</translation>
+        </message>
+        <message>
+            <source>No glass or hat as it might affect the recognition rate</source>
+            <translation>Не одевайте очки или головной убор ,это может помешать уровню распознания</translation>
+        </message>
+        <message>
+            <source>2</source>
+            <translation>2</translation>
+        </message>
+        <message>
+            <source>Capture</source>
+            <translation>Съёмка</translation>
+        </message>
+        <message>
+            <source>Train Your Face</source>
+            <translation type="unfinished">
+            </translation>
+        </message>
+        <message>
+            <source>Back</source>
+            <translation>Назад</translation>
+        </message>
+        <message>
+            <source>Advanced Settings</source>
+            <translation>Дополнительные настройки</translation>
+        </message>
+        <message>
+            <source>About this Software !</source>
+            <translation>О программе !</translation>
+        </message>
+        <message>
+            <source>background-color: rgb(0, 0, 0);</source>
+            <translation>background-color: rgb(0, 0, 0);</translation>
+        </message>
+        <message>
+            <source>Delete Selected Item</source>
+            <translation>Удалить выбранный экземпляр</translation>
+        </message>
+        <message>
+            <source>3</source>
+            <translation>3</translation>
+        </message>
+        <message>
+            <source>Congratulations</source>
+            <translation>Поздравления</translation>
+        </message>
+        <message>
+            <source>Get ready to be dazzled!</source>
+            <translation>Приготовьтесь быть ослепленными!</translation>
+        </message>
+        <message>
+            <source>Finish</source>
+            <translation>Закончить</translation>
+        </message>
+        <message>
+            <source>Thank you for using our application, to be a part of this project , visit http://code.google.com/p/pam-face-authentication/ , we hope to see you there!
+regards
+Developers</source>
+            <translation>Благодарим за использование нашей программы , чтобы принять участие в проекте посетите http://code.google.com/p/pam-face-authentication/ , надеемся увидеть вас там!
+благодарности
+Разработчики</translation>
+        </message>
+        <message>
+            <source>Cancel</source>
+            <translation>Отменить</translation>
+        </message>
+        <message>
+            <source>Please come closer to the camera.</source>
+            <translation>Пожалуйста приблизьтесь к камере.</translation>
+        </message>
+        <message>
+            <source>Please go little far from the camera.</source>
+            <translation>Пожалуйста отдалитесь от камеры.</translation>
+        </message>
+        <message>
+            <source>Unable to Detect Your Face.</source>
+            <translation>Не получается определить ваше лицо.</translation>
+        </message>
+        <message>
+            <source>Tracker lost, trying to reinitialize.</source>
+            <translation>Трэкер потерян, пытаюсь перезапустить.</translation>
+        </message>
+        <message>
+            <source>Tracking in progress.</source>
+            <translation>Отслеживание в процессе.</translation>
+        </message>
+        <message>
+            <source>Captured %1 / %2 faces.</source>
+            <translation>Снято %1 из %2 изображений.</translation>
+        </message>
+        <message>
+            <source>Capturing Image Finished.</source>
+            <translation>Съёма завершенаю</translation>
+        </message>
+        <message>
+            <source>Processing Faces, Please Wait ...</source>
+            <translation>Обработка Лица ,Пожалуйста Подождите ...</translation>
+        </message>
+        <message>
+            <source>Processing Completed.</source>
+            <translation>Процесс Завершён.</translation>
+        </message>
+        <message>
+            <source>&lt;strong&gt;Error&lt;/strong&gt;</source>
+            <translation>&lt;strong&gt;Ошибка&lt;/strong&gt;</translation>
+        </message>
+        <message>
+            <source>Camera Not Found. &lt;br /&gt; Plugin Your Camera and Try Again.</source>
+            <translation>Камера не найдена. &lt;br /&gt; Подключите Камеру и попробуйте заново.</translation>
+        </message>
+    </context>
+    <context>
+        <name>faceTrainerAdvSettings</name>
+        <message>
+            <source>Advanced Settings</source>
+            <translation>Дополнительные Настройки</translation>
+        </message>
+        <message>
+            <source>Low Security</source>
+            <translation>Низкий Уровень Безопасности</translation>
+        </message>
+        <message>
+            <source>High Security</source>
+            <translation>Высокий Уровень Безопасности</translation>
+        </message>
+        <message>
+            <source>background-color: rgb(0, 0, 0);</source>
+            <translation>background-color: rgb(0, 0, 0);</translation>
+        </message>
+        <message>
+            <source>Test Recognition</source>
+            <translation>Тестовое Распознание</translation>
+        </message>
+        <message>
+            <source>Yes/No</source>
+            <translation>Да/Нет</translation>
+        </message>
+        <message>
+            <source>Test Recognition Result:</source>
+            <translation>Результат теста:</translation>
+        </message>
+        <message>
+            <source>Save Config</source>
+            <translation>Сохранить настройки</translation>
+        </message>
+        <message>
+            <source>Defaults</source>
+            <translation>По умолчанию</translation>
+        </message>
+        <message>
+            <source>Medium</source>
+            <translation>Средний</translation>
+        </message>
+        <message>
+            <source>Yes</source>
+            <translation>Да</translation>
+        </message>
+        <message>
+            <source>No</source>
+            <translation>Нет</translation>
+        </message>
+    </context>
+</TS>
