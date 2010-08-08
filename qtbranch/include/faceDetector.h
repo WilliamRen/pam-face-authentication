3645263f3ddd935ac4d482e11d7402b0114d71b6
feichtnerjoh@gmail.com <feichtnerjoh@gmail.com@23b13dde-1d4e-0410-9555-c5db500eae70>
2010-08-08 21:11:30 +0000

    Refurbished faceDetector

--- qtbranch/include/faceDetector.h
+++ qtbranch/include/faceDetector.h
@@ -1,57 +1,85 @@
-/** @file */
+#ifndef _INCL_FACE_DETECTOR
+#define _INCL_FACE_DETECTOR
 
 #include "cv.h"
 #include "highgui.h"
+
 /**
-* Face Structure, Information that can describe the detected Face on the Image
+* Face structure, information that can describe the detected face on the image
 */
 struct face
 {
-    CvPoint LT; /**< Co-ordinates of Left Top of the Face */
-    CvPoint RB;/**< Co-ordinates of Right Top of the Face */
-    int Width;/**< Width of the Face */
-    int Height;/**< Height of the Face */
-} ;
+  CvPoint LT;  /**< Co-ordinates of Left Top of the Face */
+  CvPoint RB;  /**< Co-ordinates of Right Top of the Face */
+  int Width;   /**< Width of the Face */
+  int Height;  /**< Height of the Face */
+};
+
 /**
-* Face Detector Class. This Class Runs the OpenCV Haar Detect Functions for finding the Face.
+* Face Detector class. This class runs the OpenCV Haar detect functions 
+* for finding the face.
 */
 class faceDetector
 {
-public:
+  private:
     /**
-    * Face Information struct.
-    *@see struct face
-    */
-    struct face faceInformation;
-    /**
-    * The Constructor
-    *@see Initializes Structures for Haar Detection
-    */
-    faceDetector(void);
-    /**
-    *Runs the Face Detection Algorithm on the param image
-    *@param input The Input image on which the algorithm should be run on.
-    */
-    void runFaceDetector(IplImage * input);
-    /**
-    *Returns the Face Image of the Detected Face
-    *@param inputImage The Input image.
-    *@result IplImage on success , 0 on failure
-    */
-    IplImage * clipDetectedFace(IplImage * inputImage);
-    /**
-    *Returns 1 or 0 , depending on Success or Failure of the detection algorithm
-    *@result 1 on success , 0 on failure
-    */
-    int checkFaceDetected();
-private:
-    /**
-    *OpenCV Cascade Structure
+    *OpenCV cascade structure
     */
     CvHaarClassifierCascade* cascade;
+    
     /**
-    *Storage - Work Area for the OpenCV detection algorithm
+    * Storage - Work area for the OpenCV detection algorithm
     */
     CvMemStorage* storage;
 
+    /**
+    * Copy constructor
+    * @param faceDetector, original value to copy
+    */
+    faceDetector(faceDetector&);
+
+    /**
+    * Assignment operator
+    * @param faceDetector, original value to assign
+    */
+    faceDetector& operator =(const faceDetector&);
+
+  public:
+    /**
+    * Face information struct.
+    * @see struct face
+    */
+    struct face faceInformation;
+
+    /**
+    * The Constructor
+    * @see Initializes structures for Haar detection
+    */
+    faceDetector();
+
+    /**
+    * The destructor
+    */
+    virtual ~faceDetector();
+
+    /**
+    * Runs the face detection algorithm on the param image
+    * @param input, The input image on which the algorithm should be run on.
+    */
+    void runFaceDetector(IplImage* input);
+    
+    /**
+    * Returns the Face image of the detected face
+    * @param inputImage, The Input image.
+    * @result IplImage on success, 0 on failure
+    */
+    IplImage* clipDetectedFace(IplImage* inputImage);
+    
+    /**
+    * Returns 1 or 0 , depending on Success or Failure of the detection algorithm
+    * @result true on success, false on failure
+    */
+    bool checkFaceDetected();
 };
+
+#endif // _INCL_FACE_DETECTOR
