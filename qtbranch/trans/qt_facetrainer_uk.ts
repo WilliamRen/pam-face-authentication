af7e0ca73ad0582afeca54987432787dc3a06cc7
rohan.anil <rohan.anil@23b13dde-1d4e-0410-9555-c5db500eae70>
2010-08-08 12:16:51 +0000

    Ukranian Translation added

---
+++ qtbranch/trans/qt_facetrainer_uk.ts
@@ -0,0 +1,238 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS>
+    <context>
+        <name>aboutDialog</name>
+        <message>
+            <source>About the Software \m/</source>
+            <translation>Про програму \m/</translation>
+        </message>
+        <message>
+            <source>Pluggable Authentication Module for your favorite Linux Distro</source>
+            <translation>PAM модуль для вашого улюбленого Linux дистрибутиву</translation>
+        </message>
+        <message>
+            <source>About</source>
+            <translation>Детальніше</translation>
+        </message>
+        <message>
+            <source>This program allows you to login by Face Authentication.
+Care has been taken to give you the best User Experience Possible.
+If you encounter any bugs , report at  </source>
+            <translation>Ця програма дозволить вам входити в систему, використовуючи ідентифікацію лиця особи.
+Повідомляйте про помилки на</translation>
+        </message>
+        <message>
+            <source>This program was developed during the auspicious Google Summer of Code program in 2009 and 2008
+</source>
+            <translation>Ця програма була розроблена під час Google Summer of Code у 2008 та 2009 роках
+</translation>
+        </message>
+        <message>
+            <source>Authors</source>
+            <translation>Автори</translation>
+        </message>
+        <message>
+            <source>Thanks To</source>
+            <translation>Спасибі</translation>
+        </message>
+        <message>
+            <source>Translation</source>
+            <translation>Переклад</translation>
+        </message>
+        <message>
+            <source>Artwork</source>
+            <translation>Дизайн</translation>
+        </message>
+        <message>
+            <source>OK</source>
+            <translation>Добре</translation>
+        </message>
+    </context>
+    <context>
+        <name>faceTrainer</name>
+        <message>
+            <source>Face Trainer</source>
+            <translation>Сканування лиця</translation>
+        </message>
+        <message>
+            <source>Next</source>
+            <translation>Далі</translation>
+        </message>
+        <message>
+            <source>For Your Information</source>
+            <translation>До вашого відома</translation>
+        </message>
+        <message>
+            <source>1</source>
+            <translation>1</translation>
+        </message>
+        <message>
+            <source>Exit</source>
+            <translation>Вихід</translation>
+        </message>
+        <message>
+            <source>Read the instructions for High Efficiency</source>
+            <translation>Прочитайте інструкцію</translation>
+        </message>
+        <message>
+            <source>To ensure high efficiency please keep proper distance between face and webcam</source>
+            <translation>Для забезпечення високої ефективності підтримуйте належну відстань між лицем та веб-камерою</translation>
+        </message>
+        <message>
+            <source>Try to sit where there is ample lighting</source>
+            <translation>Спробуйте сісти там, де є достатнє освітлення</translation>
+        </message>
+        <message>
+            <source>No glass or hat as it might affect the recognition rate</source>
+            <translation>Зніміть окуляри та головний убір, так як це може вплинути на швидкість розпізнавання</translation>
+        </message>
+        <message>
+            <source>2</source>
+            <translation>2</translation>
+        </message>
+        <message>
+            <source>Capture</source>
+            <translation>Захоплення</translation>
+        </message>
+        <message>
+            <source>Train Your Face</source>
+            <translation>Сканувати ваше обличчя</translation>
+        </message>
+        <message>
+            <source>Back</source>
+            <translation>Назад</translation>
+        </message>
+        <message>
+            <source>Advanced Settings</source>
+            <translation>Додаткові налаштування</translation>
+        </message>
+        <message>
+            <source>About this Software !</source>
+            <translation>Про програму</translation>
+        </message>
+        <message>
+            <source>Delete Selected Item</source>
+            <translation>Видалити вибране фото</translation>
+        </message>
+        <message>
+            <source>3</source>
+            <translation>3</translation>
+        </message>
+        <message>
+            <source>Congratulations</source>
+            <translation>Вітаємо!</translation>
+        </message>
+        <message>
+            <source>Get ready to be dazzled!</source>
+            <translation>Приготуйтесь!</translation>
+        </message>
+        <message>
+            <source>Finish</source>
+            <translation>Закінчити</translation>
+        </message>
+        <message>
+             <source>Thank you for using our application, to be a part of this project , visit http://code.google.com/p/pam-face-authentication/ , we hope to see you there!
+
+
+regards
+Developers</source>
+            <translation>Дякуємо за використання нашого програмного забезпечення, відвідуйте наш сайт: http://code.google.com/p/pam-face-authentication/</translation>
+        </message>
+        <message>
+            <source>Cancel</source>
+            <translation>Скасувати</translation>
+        </message>
+        <message>
+            <source>Please come closer to the camera.</source>
+            <translation>Будь ласка, посуньтеся ближче до камери.</translation>
+        </message>
+        <message>
+            <source>Please go little far from the camera.</source>
+            <translation>Будь ласка, відсуньте від камери.</translation>
+        </message>
+        <message>
+            <source>Unable to Detect Your Face.</source>
+            <translation>Не вдалося визначити ваше обличчя.</translation>
+        </message>
+        <message>
+            <source>Tracker lost, trying to reinitialize.</source>
+            <translation>Спроба повторної ініціалізації.</translation>
+        </message>
+        <message>
+            <source>Tracking in progress.</source>
+            <translation>Слідкування за прогресом.</translation>
+        </message>
+        <message>
+            <source>Captured %1 / %2 faces.</source>
+            <translation>Записано %1 / %2 лиця.</translation>
+        </message>
+        <message>
+            <source>Capturing Image Finished.</source>
+            <translation>Захоплення зображення закінчено.</translation>
+        </message>
+        <message>
+            <source>Processing Faces, Please Wait ...</source>
+            <translation>Обробка лиця, будь ласка зачекайте ...</translation>
+        </message>
+        <message>
+            <source>Processing Completed.</source>
+            <translation>Обробка закінчена.</translation>
+        </message>
+        <message>
+            <source>&lt;strong&gt;Error&lt;/strong&gt;</source>
+            <translation>&lt;strong&gt;Помилка&lt;/strong&gt;</translation>
+        </message>
+        <message>
+            <source>Camera Not Found. &lt;br /&gt; Plugin Your Camera and Try Again.</source>
+            <translation>Камера не знайдена. &lt;br /&gt; Підключіть вашу камеру і спробуйте знову.</translation>
+        </message>
+    </context>
+    <context>
+        <name>faceTrainerAdvSettings</name>
+        <message>
+            <source>Advanced Settings</source>
+            <translation>Додаткові налаштування</translation>
+        </message>
+        <message>
+            <source>Low Security</source>
+            <translation>Низька безпека</translation>
+        </message>
+        <message>
+            <source>High Security</source>
+            <translation>Висока безпека</translation>
+        </message>
+        <message>
+            <source>Test Recognition</source>
+            <translation>Тест розпізнавання</translation>
+        </message>
+        <message>
+            <source>Yes/No</source>
+            <translation>Так/Ні</translation>
+        </message>
+        <message>
+            <source>Test Recognition Result:</source>
+            <translation>Результат розпізнавання:</translation>
+        </message>
+        <message>
+            <source>Save Config</source>
+            <translation>Зберегти налаштування</translation>
+        </message>
+        <message>
+            <source>Defaults</source>
+            <translation>За замовчуванням</translation>
+        </message>
+        <message>
+            <source>Medium</source>
+            <translation>Помірний</translation>
+        </message>
+        <message>
+            <source>Yes</source>
+            <translation>Так</translation>
+        </message>
+        <message>
+            <source>No</source>
+            <translation>Ні</translation>
+        </message>
+    </context>
+</TS>
